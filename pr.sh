aabaac4b6dc96b6b43ccf2d3d4b17acd0041c5a3
Jason Smith <jasonrichardsmith@gmail.com>
2018-11-01 16:04:36 +0100

    test automated kre push

---
+++ pr.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+HUB="hub-linux-amd64-2.6.0"
+CURRENT_DIR=$(pwd)
+curl -O -L https://github.com/github/hub/releases/download/v2.6.0/${HUB}.tgz
+tar -xzvf ${HUB}.tgz
+export PATH=${PATH}:${CURRENT_DIR}/${HUB}/bin
+export WINDOWS_SHA=$(sha256sum bin/windows/rbac-view | awk '{ print $1 }' )
+export LINUX_SHA=$(sha256sum bin/linux/rbac-view | awk '{ print $1 }' )
+export DARWIN_SHA=$(sha256sum bin/darwin/rbac-view | awk '{ print $1 }' )
+git clone git@github.com:jasonrichardsmith/krew-index.git
+cat rbac-view.krew.template.yaml | envsubst > krew-index/plugins/rbac-view.yaml
+cd krew-index
+git checkout -b ${TRAVIS_TAG}
+git add plugins/rbac-view.yaml
+git commit -m 'Release ${TRAVIS_TAG}'
+git push origin ${TRAVIS_TAG} --force
+hub pull-request \
+	--base="GoogleContainerTools:master" \
+	--head="jasonrichardsmith/krew-index:${TRAVIS_TAG}" \
+	--message="Update rbac-view ${TRAVIS_TAG}"
+cd ../
+rm -rf krew-index ${HUB} ${HUB}.tgz
