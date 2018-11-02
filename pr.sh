cdbb33ffc3c637977d75c4101dc2db322fd8d3e4
Jason Smith <jasonrichardsmith@gmail.com>
2018-11-02 11:52:00 +0100

    trying to auth with token

--- pr.sh
+++ pr.sh
@@ -7,13 +7,15 @@ export PATH=${PATH}:${CURRENT_DIR}/${HUB}/bin
 export WINDOWS_SHA=$(sha256sum bin/windows/rbac-view | awk '{ print $1 }' )
 export LINUX_SHA=$(sha256sum bin/linux/rbac-view | awk '{ print $1 }' )
 export DARWIN_SHA=$(sha256sum bin/darwin/rbac-view | awk '{ print $1 }' )
-hub clone https://github.com/jasonrichardsmith/krew-index.git
+git clone https://github.com/jasonrichardsmith/krew-index.git
 cat rbac-view.krew.template.yaml | envsubst > krew-index/plugins/rbac-view.yaml
 cd krew-index
-hub checkout -b ${TRAVIS_TAG}
-hub add plugins/rbac-view.yaml
-hub commit -m 'Release ${TRAVIS_TAG}'
-hub push origin ${TRAVIS_TAG} --force
+git checkout -b ${TRAVIS_TAG}
+git add plugins/rbac-view.yaml
+git commit -m 'Release ${TRAVIS_TAG}'
+git remote add krew-index https://${GITHUB_TOKEN}@jasonrichardsmith/krew-index.git > /dev/null 2>&1
+git push --quiet --set-upstream krew-index ${TRAVIS_TAG} --force
+
 hub pull-request \
 	--base="GoogleContainerTools:master" \
 	--head="jasonrichardsmith/krew-index:${TRAVIS_TAG}" \
